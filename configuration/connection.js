ce96b41305ab96e2383879b0a68d5548525979be
Abhijith Suresh <abhijithsuresh700@gmail.com>
2022-10-05 20:08:38 +0530

    atlas

--- configuration/connection.js
+++ configuration/connection.js
@@ -4,7 +4,8 @@ const state={
 }
 
 module.exports.connect=function(done){
-    const url='mongodb://localhost:27017'
+    //const url='mongodb://localhost:27017'
+    const url='mongodb+srv://mysundrymails700:Z3LdyfaGqpy5oJbM@cluster0.fhx2ga7.mongodb.net/?retryWrites=true&w=majority'
     const dbname='NileShopping'
 
     mongoClient.connect(url,(err,data)=>{
